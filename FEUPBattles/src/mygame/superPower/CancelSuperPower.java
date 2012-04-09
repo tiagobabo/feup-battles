54a43a76281bc56980832511498e9b3ee38988a2
JosePedroMarques <ze.p.smarques@gmail.com>
2012-04-09 16:00:25 +0100

    tirado os print

--- FEUPBattles/src/mygame/superPower/CancelSuperPower.java
+++ FEUPBattles/src/mygame/superPower/CancelSuperPower.java
@@ -29,7 +29,7 @@ public class CancelSuperPower extends Thread {
             Thread.sleep(waitTime*1000);
             power.cancelSuperPower(pNum);
             power.setInUse(false);
-            System.out.println("POWER CANCELED");
+            
         } catch (InterruptedException ex) {
             Logger.getLogger(CancelSuperPower.class.getName()).log(Level.SEVERE, null, ex);
         }
